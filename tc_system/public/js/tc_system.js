0f4c963e66ba1eceb5f8953e0c3f55a28c8128b8
TangGroup <68223882+khengleng855@users.noreply.github.com>
2026-01-10 14:14:31 +0700

    feat: Restrict Help Menu to only TC System Support links

--- tc_system/public/js/tc_system.js
+++ tc_system/public/js/tc_system.js
@@ -5,18 +5,17 @@ $(document).on("app_ready", function () {
     setTimeout(() => {
         let help_dropdown = $('.dropdown-help .dropdown-menu');
         if (help_dropdown.length) {
-            // Option 1: Clear existing (Documentation, Forum, etc.)
-            // help_dropdown.empty(); 
+            // Clear existing (Documentation, Forum, etc.)
+            help_dropdown.empty();
 
-            // Option 2: Prepend our custom links
+            // Add our custom links
             let custom_help = `
                 <li class="dropdown-header">Telecom Cambodia</li>
                 <li><a class="dropdown-item" href="https://wiki.telecomcambodia.com" target="_blank">Platform Manual</a></li>
                 <li><a class="dropdown-item" href="mailto:support@telecomcambodia.com">Contact IT Ops</a></li>
-                <li class="dropdown-divider"></li>
             `;
 
-            help_dropdown.prepend(custom_help);
+            help_dropdown.append(custom_help);
         }
     }, 1000);
 });
