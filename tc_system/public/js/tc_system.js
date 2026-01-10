e11131c13f1095c17cfe425e8b51898ddb410030
TangGroup <68223882+khengleng855@users.noreply.github.com>
2026-01-10 20:05:52 +0700

    Day 2 Fix: Force removal of default Help Menu items via CSS/JS

--- tc_system/public/js/tc_system.js
+++ tc_system/public/js/tc_system.js
@@ -1,39 +1,39 @@
 frappe.provide("tc_system");
 
 $(document).on("app_ready", function () {
-    // Override Help Menu - Use Interval to ensure it catches late rendering
     let retry_count = 0;
     const cleanHelpMenu = setInterval(() => {
         let help_dropdown = $('.dropdown-help .dropdown-menu');
 
-        // Find if there are standard items to remove (or just ensure ours are the only ones)
         if (help_dropdown.length) {
+            // Check if we need to clean (if it has items that are NOT ours)
+            // We define ours by class 'tc-safe'
+            if (help_dropdown.children(':not(.tc-safe)').length > 0) {
 
-            // Check if it still has Frappe defaults (like "Documentation")
-            let hasDefaults = help_dropdown.find('a[href*="frappeframework.com"]').length > 0;
-
-            if (hasDefaults || help_dropdown.children().length > 3) {
-                // Wipe it clean
+                // Remove anything that isn't ours
+                // Actually, empty() and rebuild is safer to ensure order
                 help_dropdown.empty();
 
-                // Re-add OUR Branding
+                // Re-add OUR Branding with 'tc-safe' class
                 let custom_help = `
-                     <li class="dropdown-header">Telecom Cambodia Operations</li>
-                     <li><a class="dropdown-item" href="#" onclick="frappe.msgprint('Manual Coming Soon')">Platform Manual</a></li>
-                     <li><a class="dropdown-item" href="mailto:support@telecomcambodia.com">Contact IT Ops</a></li>
+                     <li class="dropdown-header tc-safe">Telecom Cambodia Operations</li>
+                     <li class="tc-safe"><a class="dropdown-item" href="#" onclick="frappe.msgprint('Manual Coming Soon')">Platform Manual</a></li>
+                     <li class="tc-safe"><a class="dropdown-item" href="mailto:support@telecomcambodia.com">Contact IT Ops</a></li>
                  `;
                 help_dropdown.append(custom_help);
+
+                // Also try to forcefully remove specific selectors if they are stubborn
+                $('.dropdown-help .dropdown-menu > li:not(.tc-safe)').remove();
+
                 console.log("TC System: Help Menu Sanitized");
             }
         }
 
         retry_count++;
-        // Stop checking after 10 seconds of app load
         if (retry_count > 20) clearInterval(cleanHelpMenu);
     }, 500);
 });
 
-// Also customize the generic "About" dialog
 frappe.provide('frappe.ui.misc');
 frappe.ui.misc.about = function () {
     frappe.msgprint({
