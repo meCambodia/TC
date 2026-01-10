9e368af3a812c821c8218b2469b4f0c928acb9f2
TangGroup <68223882+khengleng855@users.noreply.github.com>
2026-01-10 20:12:07 +0700

    Day 2 Fix: Enable CSS on Web Context and Apply Global Navbar Cleanup

--- tc_system/hooks.py
+++ tc_system/hooks.py
@@ -18,12 +18,13 @@ website_context = {
 # ------------------
 
 # include js, css files in header of desk.html
+# include js, css files in header of desk.html
 app_include_js = "/assets/tc_system/js/tc_system.js"
 app_include_css = "/assets/tc_system/css/tc_system.css"
 
 # include js, css files in header of web template
-# web_include_js = "/assets/tc_system/js/tc_system_web.js"
-# web_include_css = "/assets/tc_system/css/tc_system_web.css"
+web_include_js = "/assets/tc_system/js/tc_system.js"
+web_include_css = "/assets/tc_system/css/tc_system.css"
 
 # Authentication and Authorization
 # --------------------------------
